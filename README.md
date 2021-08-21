dfaa426c44f66d33f2d07566b7768a14b11f16ec
allprecisely <allprecisely@gmail.com>
2021-08-21 22:34:51 +0300

    edit css file

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 Небольшое погружение во flask с https://www.youtube.com/watch?v=Z1RJmh_OqeA
 
 Проект залит на heroku: https://giiflaskapp.herokuapp.com/
-
-Хотелось бы доработать:
-1) .css файлик
--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask import redirect
 from flask import request
 from flask import render_template
-from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
